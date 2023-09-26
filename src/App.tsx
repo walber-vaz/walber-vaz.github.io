54af851fcd9d9c25afcab862f8951635f0e5b260
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-09-25 22:15:14 -0300

    feat: create main home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+
 export const App: React.FC = (): JSX.Element => {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">App</h1>
+    <main
+      className={cn(
+        'flex',
+        'flex-col',
+        'items-center',
+        'justify-center',
+        'min-h-screen',
+        'h-screen',
+        'w-[1280px]',
+        'mx-auto',
+      )}
+    >
+      <h1 className={cn('text-6xl', 'text-center', 'text-green-500')}>
+        Walber Vaz
+      </h1>
+      <Button
+        className={cn(
+          'mt-4',
+          'px-4',
+          'py-2',
+          'text-lg',
+          'bg-transparent',
+          'border-2',
+          'border-solid',
+          'rounded-md',
+          'transition-colors',
+          'duration-300',
+          'ease-in-out',
+          'text-green-500',
+          'border-green-500',
+          'hover:bg-green-500',
+          'hover:text-gray-900',
+        )}
+      >
+        Entre em contato
+      </Button>
     </main>
   )
 }
